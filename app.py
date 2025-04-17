3596021af5dc8ccb0baf0e039b91328b6f31d8aa
Dheeraj Kurupati <117580661+Dheerajkurupati@users.noreply.github.com>
2025-04-17 11:26:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,11 +40,5 @@ def predict():
 
     return jsonify({'prediction': round(prediction, 2)})
 
-
-@app.route('/health')
-def health():
-    return "OK", 200
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000, debug=False)
